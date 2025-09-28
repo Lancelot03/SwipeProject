1c9f5a59234d738d7a83f11a60ce9d8af96cd6f9
Harsh <90236987+Lancelot03@users.noreply.github.com>
2025-09-28 13:08:24 +0530

    Update ResumeUpload.jsx

--- src/components/ResumeUpload.jsx
+++ src/components/ResumeUpload.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import { Upload, Button, message } from 'antd'
+import { InboxOutlined } from '@ant-design/icons'
+import { extractTextFromPDF, extractTextFromDocx, extractNameEmailPhone } from '../utils/resumeParser'
+import { useDispatch } from 'react-redux'
+import { createCandidate, updateProfile } from '../features/candidatesSlice'
 
+
+export default function ResumeUpload() {
+const dispatch = useDispatch()
+
+
+const handleFile = async (file) => {
+try {
+let text = ''
+if (file.type === 'application/pdf' || file.name.endsWith('.pdf'))
