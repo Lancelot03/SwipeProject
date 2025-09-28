3e12c0a8caa382cc47ea31376a3b96a99a70e38f
Harsh <90236987+Lancelot03@users.noreply.github.com>
2025-09-28 13:49:11 +0530

    Update InterviewChat.jsx

--- src/components/InterviewChat.jsx
+++ src/components/InterviewChat.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
 
+export default function InterviewChat({ candidateId }) {
+  return (
+    <div>
+      <h3>Interview Chat Placeholder</h3>
+      <p>Select a candidate to start the interview.</p>
+    </div>
+  )
+}
