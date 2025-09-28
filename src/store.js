839986ad74f561442f34b76a003e1da24565d267
Harsh <90236987+Lancelot03@users.noreply.github.com>
2025-09-28 12:41:57 +0530

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import candidatesReducer from './features/candidatesSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+
+const rootReducer = combineReducers({ candidates: candidatesReducer })
+
+
+const persistConfig = { key: 'root', storage }
+const persisted = persistReducer(persistConfig, rootReducer)
+
+
+export const store = configureStore({ reducer: persisted })
+export const persistor = persistStore(store)
